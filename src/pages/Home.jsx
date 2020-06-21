b91e01f5fcd101db66988a456d9c82215946a98b
aman-atg <amanansari.a2@gmail.com>
2020-06-21 23:13:43 +0530

    add more content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+// use media.match or/and lazy loading..
+import { ReactComponent as HeaderBgDesktop } from "../assets/images/bg-header-desktop.svg";
+import { ReactComponent as HeaderBgMobile } from "../assets/images/bg-header-mobile.svg";
+import CardList from "../components/CardList";
+
 const Home = () => {
-  return <div className="Home">Hello World</div>;
+  return (
+    <div className="Home">
+      <div className="header">
+        <HeaderBgDesktop />
+      </div>
+      <div className="body">
+        <CardList />
+      </div>
+    </div>
+  );
 };
 
 export default Home;

2164b2d7f985a1f4fb6aa8073ef83f25aabfbf15
aman-atg <amanansari.a2@gmail.com>
2020-06-21 23:13:15 +0530

    create

---
+++ src/components/CardList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Data from "../assets/data.json";
+
+const CardList = () => {
+  console.log(Data);
+
+  return <div className="CardList"></div>;
+};
+
+export default CardList;

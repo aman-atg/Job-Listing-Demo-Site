f2705772b56972c62f89b0135c3a0b796ad60167
aman-atg <amanansari.a2@gmail.com>
2020-06-22 15:29:06 +0530

    initial markup

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 
 const Card = props => {
-  const { company, contract, featured, id, languages } = props;
+  const {
+    company,
+    logo,
+    featured,
+    position,
+    role,
+    level,
+    postedAt,
+    contract,
+    location,
+    languages,
+    tools,
+  } = props;
 
-  return <div className="Card">{company}</div>;
+  return (
+    <div className="Card">
+      <div className="Card-header">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="Card-body">
+        <div className="heading">
+          <span className="company">{company} </span>
+          <div className="labels">
+            New<span>{featured}</span>
+          </div>
+        </div>
+        <h1>{position}</h1>
+        <div className="footer">
+          {postedAt} {contract} {location}
+        </div>
+      </div>
+
+      <div className="Card-footer">{languages}</div>
+    </div>
+  );
 };
 
 export default Card;

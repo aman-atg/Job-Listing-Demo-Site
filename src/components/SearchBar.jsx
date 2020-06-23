501417c4e05409c9e199c03986edbf72ea8ee5f7
aman-atg <amanansari.a2@gmail.com>
2020-06-24 04:44:59 +0530

    add X

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-
 const SearchBar = ({ terms, clear }) => {
   return (
     <div className="SearchBar">
       <div className="terms">
         {terms.map((term, i) => (
           <p key={i}>
-            {term} <span onClick={() => clear(term)}>X</span>
+            {term}
+            <span onClick={() => clear(term)}>❌</span>
           </p>
         ))}
       </div>
-      <button onClick={() => clear("")}>clear</button>
+      <button className="clear" onClick={() => clear("")}>
+        clear
+      </button>
     </div>
   );
 };

27a7530d7c12349361e1f0ad78d90e59347886a5
aman-atg <amanansari.a2@gmail.com>
2020-06-23 23:14:29 +0530

    logic for filtering Cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,15 +13,26 @@ const Home = () => {
     setTerms([...new Set([...terms, term])]);
   };
   const clear = term => {
-    console.log(term);
-
     if (term === "") setTerms([]);
     else setTerms(terms.filter(t => t !== term));
   };
 
-  const Cards = Data.map(card => (
-    <Card addTerm={term => addTerm(term)} key={card.id} {...card} />
-  ));
+  const Cards = Data.map(card => {
+    if (terms.length === 0) {
+      return <Card addTerm={term => addTerm(term)} key={card.id} {...card} />;
+    }
+    let flg = 1;
+    const { role, level, languages, tools } = card;
+    const allTerms = [role, level, ...languages, ...tools];
+
+    terms.forEach(t => {
+      if (allTerms.filter(term => t === term).length === 0) flg = 0;
+    });
+
+    return flg ? (
+      <Card addTerm={term => addTerm(term)} key={card.id} {...card} />
+    ) : null;
+  });
   return (
     <div className="Home">
       <div className="header"></div>

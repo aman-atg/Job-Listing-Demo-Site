a59a0e500b3304f15645a1c3f4446baa3b3049e6
aman-atg <amanansari.a2@gmail.com>
2020-06-23 18:25:09 +0530

    update

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -4,7 +4,6 @@ import Data from "../assets/data.json";
 import Card from "./Card";
 
 const CardList = () => {
-  console.log(Data);
   const Cards = Data.map(card => <Card key={card.id} {...card} />);
   return <div className="CardList"> {Cards} </div>;
 };

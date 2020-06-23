16e6d171df2f61ed93b648a6d8dddf540acfbad3
aman-atg <amanansari.a2@gmail.com>
2020-06-23 22:39:27 +0530

    refactor + enhancements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
-import CardList from "../components/CardList";
+import Card from "../components/Card";
+import SearchBar from "../components/SearchBar";
+
+import Data from "../assets/data.json";
 
 const Home = () => {
+  const [terms, setTerms] = useState([]);
+
+  const addTerm = term => {
+    //unique terms only
+    setTerms([...new Set([...terms, term])]);
+  };
+  const clear = term => {
+    console.log(term);
+
+    if (term === "") setTerms([]);
+    else setTerms(terms.filter(t => t !== term));
+  };
+
+  const Cards = Data.map(card => (
+    <Card addTerm={term => addTerm(term)} key={card.id} {...card} />
+  ));
   return (
     <div className="Home">
       <div className="header"></div>
+      <SearchBar terms={terms} clear={clear} />
       <div className="body">
-        <CardList />
+        <div className="CardList">{Cards}</div>
       </div>
     </div>
   );
